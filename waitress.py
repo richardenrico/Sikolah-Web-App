1ec6a7d7969f489a93e4a59d18d42d71819b8478
Richard Enrico <richardenrico02@gmail.com>
2020-12-18 23:36:32 +0800

    Update waitress.py

--- waitress.py
+++ waitress.py
@@ -2,5 +2,5 @@ from sikolah import app
 import waitress
 
 if name == __main__:
-    port = int(os.eniron.get('PORT', 33507)))
+    port = int(os.eniron.get('PORT', 33507))
     waitress.server(app, port=port)
